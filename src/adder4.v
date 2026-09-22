4e7dedaeafe7813fc24e3989558ac54cca7fe1f3
zhenmingl-school <zhenmingl@cpp.edu>
2026-09-21 17:25:39 -0700

    Add 4-bit adder functionality using full adders
    Implemented a 4-bit adder using four instances of the full adder module.

--- src/adder4.v
+++ src/adder4.v
@@ -13,6 +13,41 @@ module adder4(
 	      output [4:0] result);
    
 // add your code here -- you must use four instances of the full adder "fulladd", defined below
+	wire cout0, cout1, cout2, cout3;
+	
+	fulladd addc0(
+		.x(a[0]),
+		.y(b[0]),
+		.cin(1'b0),
+		.sum(result(0)),
+		.cout(cout0)
+	);
+	
+		fulladd addc1(
+		.x(a[1]),
+		.y(b[2]),
+		.cin(cout0),
+		.sum(result(1)),
+		.cout(cout1)
+	);
+	
+		fulladd addc2(
+			.x(a[2]),
+			.y(b[2]),
+			.cin(cout1),
+			.sum(result(2)),
+			.cout(cout2)
+	);
+	
+		fulladd addc3(
+			.x(a[3]),
+			.y(b[3]),
+			.cin(cout2),
+			.sum(result(3)),
+			.cout(cout3)
+	);
+
+	
    
 endmodule
 
